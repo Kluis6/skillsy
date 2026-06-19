24fd995bc3b89e8f8f9dec4e1a9b0dde9fb99250
kluis6 <luislmorningstar@gmail.com>
2026-06-19 09:24:57 -0300

    fix load

--- app/(inicial)/loading.tsx
+++ app/(inicial)/loading.tsx
@@ -6,38 +6,32 @@ export default function Loading() {
       <div className="container mx-auto px-4">
         {/* Navbar skeleton style */}
         <div className="flex justify-between items-center py-2">
-          <Skeleton className="size-10 rounded" />
-          <Skeleton className="h-10 w-32 rounded hidden" />
-          <div className="flex gap-2">
+          <Skeleton className="size-10 rounded md:hidden" />
+          <Skeleton className="h-10 w-32 rounded hidden md:block" />
+          <div className="flex gap-2.5">
             <Skeleton className="size-10 rounded" />
             <Skeleton className="size-10 rounded" />
-            <Skeleton className="h-10 w-32 rounded hidden" />
+            <Skeleton className="h-10 w-18 rounded hidden md:block" />
           </div>
         </div>
 
         {/* Hero skeleton style */}
-        <div className="h-[58vh] w-full flex flex-col justify-center items-center space-y-4">
-          <div className="flex flex-col gap-4 items-center justify-center w-full" >
-            <Skeleton className="h-15 w-44 rounded-sm" />
-            <Skeleton className="h-5 w-54  rounded-full" />
-            <Skeleton className="h-12 w-full rounded-full" />
+        <div className="h-[58vh] md:h-[84vh] w-full flex flex-col justify-center items-center space-y-4">
+          <div className="flex flex-col gap-4 items-center justify-center w-full">
+            <Skeleton className="h-15 md:h-32 w-44 md:w-98 rounded" />
+            <Skeleton className="h-5 w-54 md:w-90  rounded-full" />
+            <Skeleton className="h-12 w-full md:w-[42rem] rounded-full" />
             <Skeleton className="h-5 w-52  rounded-full" />
-            <Skeleton className="h-12 w-xs rounded" />
-            <Skeleton className="h-12 w-xs rounded" />
+            <div className="flex flex-col md:flex-row gap-4">
+              <Skeleton className="h-12 w-xs rounded" />
+              <Skeleton className="h-12 w-xs rounded" />
+            </div>
           </div>
         </div>
 
         {/* Categories/Content skeleton grid */}
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mb-16">
-          {[...Array(6)].map((_, i) => (
-            <Skeleton key={i} className="h-24 w-full rounded-2xl" />
-          ))}
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {[...Array(3)].map((_, i) => (
-            <Skeleton key={i} className="h-[400px] w-full rounded-[2.5rem]" />
-          ))}
+        <div className="">
+          <Skeleton className="h-52 w-full rounded" />
         </div>
       </div>
     </div>
